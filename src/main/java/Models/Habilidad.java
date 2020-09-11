607eb29b218140b0145660ca60bf2090e6ade4f7
Charlie <carlos.henriquez.r@usach.cl>
2020-09-10 21:15:33 -0300

    Base files

---
+++ src/main/java/Models/Habilidad.java
@@ -0,0 +1,26 @@
+package Models;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+public class Habilidad {
+
+    private BigDecimal id;
+    private String descrip;
+
+    public BigDecimal getId() {
+        return id;
+    }
+
+    public void setId(BigDecimal id) {
+        this.id = id;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+}
